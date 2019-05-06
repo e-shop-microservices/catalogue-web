b41888eb91788c47fd8f569d8f911be642fd7c84
Oles Pankiv <ojlesua@gmail.com>
2019-05-07 02:49:40 +0300

    Add Product and ProductContainer components

---
+++ src/components/Product.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Product.css'
+
+const Product = ({name, price, imagePath}) => {
+    return (
+        <div class="catalog-item-wrapper">
+            <div class="catalog-item">
+                <div class="catalog-item-discount-area"/>
+                <div class="catalog-item-image-area">
+                    <a href="https://google.com">
+                        <img src={imagePath}/>
+                    </a>
+                </div>
+                <div class="catalog-item-footer">
+                    <h4>
+                        <a href="https://google.com" class="catalog-item-title-link">{name}</a>
+                    </h4>
+                    <h4 class="catalog-item-price">{price}</h4>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Product;
---
+++ src/components/ProductContainer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Product from "./Product";
+import './ProductContainer.css'
+
+class ProductContainer extends Component {
+    render() {
+        return (
+            <div className="product-container">
+                <Product name="Штани Trimm Jurry" price="1234 грн." imagePath="/product.jpg"/>
+                <Product name="Штани Trimm Jurry" price="1234 грн." imagePath="/product.jpg"/>
+                <Product name="Штани Trimm Jurry" price="1234 грн." imagePath="/product.jpg"/>
+                <Product name="Штани Trimm Jurry" price="1234 грн." imagePath="/product.jpg"/>
+                <Product name="Штани Trimm Jurry" price="1234 грн." imagePath="/product.jpg"/>
+            </div>
+        );
+    }
+}
+
+export default ProductContainer;
