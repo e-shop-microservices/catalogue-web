f67099ec15742b34b0dd38cd9b56fd71668ae0c9
Oles Pankiv <ojlesua@gmail.com>
2019-05-07 19:55:33 +0300

    Fix Product styles

--- src/App.js
+++ src/App.js
@@ -2,23 +2,27 @@ import React from 'react';
 import Footer from './components/Footer'
 import Header from "./components/Header";
 import ProductContainer from "./components/ProductContainer";
+import Product from "./components/Product";
 import CategoryBanner from "./components/CategoryBanner";
 
 function App() {
     return (
         <div className="App">
-            <Header/>
+            {/*<Header/>*/}
             <ProductContainer/>
 
-            <div style={{
-                background: 'lightgray',
-                width: '500px',
-                height: '350px'
-            }}>
-                <CategoryBanner imagePath="/backpacks.jpg" bannerText="Backpacks"/>
-            </div>
 
-            <Footer/>
+            {/*<Product name="Штани Trimm Jurry" price="1234 грн." imagePath="/product.jpg"/>*/}
+
+            {/*<div style={{*/}
+            {/*    background: 'lightgray',*/}
+            {/*    width: '500px',*/}
+            {/*    height: '350px'*/}
+            {/*}}>*/}
+            {/*    <CategoryBanner imagePath="/backpacks.jpg" bannerText="Backpacks"/>*/}
+            {/*</div>*/}
+
+            {/*<Footer/>*/}
         </div>
     );
 }
--- src/components/Product.js
+++ src/components/Product.js
@@ -3,20 +3,16 @@ import './Product.css'
 
 const Product = ({name, price, imagePath}) => {
     return (
-        <div className="catalog-item-wrapper">
-            <div className="catalog-item">
-                <div className="catalog-item-discount-area"/>
-                <div className="catalog-item-image-area">
-                    <a href="https://google.com">
-                        <img src={imagePath} alt={name}/>
-                    </a>
-                </div>
-                <div className="catalog-item-footer">
-                    <h4>
-                        <a href="https://google.com" className="catalog-item-title-link">{name}</a>
-                    </h4>
-                    <h4 className="catalog-item-price">{price}</h4>
-                </div>
+        <div className="catalog-item">
+            <div className="catalog-item-discount-area"/>
+            <div className="catalog-item-image-area">
+                <a href="https://google.com">
+                    <img src={imagePath} alt={name}/>
+                </a>
+            </div>
+            <div className="catalog-item-footer">
+                <a href="https://google.com" className="catalog-item-title-link">{name}</a>
+                <span className="catalog-item-price">{price}</span>
             </div>
         </div>
     );
