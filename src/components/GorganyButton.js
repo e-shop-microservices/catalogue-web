7761c71b1f73fedd180e2726f7c9bbaa4e91d1d3
Oles Pankiv <ojlesua@gmail.com>
2019-05-14 00:57:31 +0300

    Add GorganyButton component

---
+++ src/components/GorganyButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './GorganyButton.css'
+
+const GorganyButton = ({title}) => {
+    return (
+        <button className="gorgany-button">{title}</button>
+    );
+};
+
+export default GorganyButton;
